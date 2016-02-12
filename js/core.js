0891f80640d01d7a9063ed5aebed3f9a137d5224
Muhammed Athimannil <hello@athimannil.com>
2016-02-12 14:34:51 +0400

    appointment in popup

--- js/core.js
+++ js/core.js
@@ -1,5 +1,5 @@
-angular.module('myApp', ['mwl.calendar', 'ui.bootstrap'])
-	.controller('calendarCtrl', ['$scope','moment','calendarConfig', function($scope, moment, calendarConfig){
+angular.module('myApp', ['ui.bootstrap', 'mwl.calendar', 'angularMoment'])
+	.controller('calendarCtrl', ['$scope','moment','$uibModal', function($scope, moment, $uibModal){
 		$scope.bookingPerDay = 5;
 		var mySchedule = this;
 		$scope.isCellOpen = true; // not working yet
@@ -7,56 +7,59 @@ angular.module('myApp', ['mwl.calendar', 'ui.bootstrap'])
 		$scope.calendarDate = new Date();
 		$scope.bookings = [
 			{
-				title: 'Today',
+				title: 'Hello mate',
 				type: 'info',
-				startsAt: new Date(2016,2,10, 1),
-				endsAt: new Date(2016,2,10, 2),
-				recursOn: 'month',
+				// startsAt: new Date(moment().locale("en").add(18, 'd').format("MMM DD, YYYY HH:MM")),
+				startsAt: moment().toDate(),
+				// startsAt: moment('2012-12-12', "YYYY-MM-DD").toDate(),
+				// startsAt: new Date(2016,1,3, 1),
+				// endsAt: new Date(moment().locale("en").add(18, 'd').format("MMM DD, YYYY HH:MM")),
+				endsAt: moment().toDate(),
+				// endsAt: moment('2012-12-12', "YYYY-MM-DD").toDate(),
+				// endsAt: new Date(2016,1,3, 1),
 
-				editable: false,
+				editable: true,
 				deletable: false,
 				draggable: false,
 				resizable: false,
 				incrementsBadgeTotal: true,
 				cssClass: 'a-css-class-name'
 			},{
-				title: 'Today is saturday',
+				title: 'Today is todate()',
 				type: 'info',
-				startsAt: new Date(2016,2,10, 3),
-				endsAt: new Date(2016,2,10, 4),
-				editable: false,
+				startsAt: moment().add(1, 'hours').toDate(),
+				endsAt: moment().add(1, 'hours').toDate(),
+				editable: true,
 				deletable: false,
 				draggable: true,
 				resizable: true,
 				incrementsBadgeTotal: true,
-				recursOn: 'month',
 				cssClass: 'a-css-class-name'
 			},{
 				title: 'Today is saturday',
 				type: 'info',
-				startsAt: new Date(2016,2,10, 3),
-				endsAt: new Date(2016,2,10, 4),
-				editable: false,
+				startsAt: moment().add(2, 'hours').toDate(),
+				endsAt: moment().add(2, 'hours').toDate(),
+				editable: true,
 				deletable: false,
 				draggable: true,
 				resizable: true,
 				incrementsBadgeTotal: true,
-				recursOn: 'month',
 				cssClass: 'a-css-class-name'
 			},{
 				title: 'Today is saturday',
 				type: 'info',
-				startsAt: new Date(2016,2,11, 3),
-				endsAt: new Date(2016,2,11, 4),
-				editable: false,
+				startsAt: moment().add(3, 'hours').toDate(),
+				endsAt: moment().add(3, 'hours').toDate(),
+				editable: true,
 				deletable: false,
 				draggable: true,
 				resizable: true,
 				incrementsBadgeTotal: true,
-				recursOn: 'month',
 				cssClass: 'a-css-class-name'
 			}
 		];
+
 
 		// Click on event in slide
 		$scope.bookingClicked = function(theBooking){
@@ -67,9 +70,30 @@ angular.module('myApp', ['mwl.calendar', 'ui.bootstrap'])
 		// Click on calendar date
 		$scope.dateClicked = function(theBooking){
 			console.clear();
-			console.log("hello mate");
 			console.log(theBooking);
-			console.log(theBooking.length);
+			console.log(theBooking.date);
+			console.log(moment(theBooking.date).toDate());
+			console.log("hello mate");
+			console.log(theBooking.events);
+			console.log(theBooking.events.length);
+
+		    var modalInstance = $uibModal.open({
+		      templateUrl: 'myModalContent.html',
+		      controller: 'bookingCtrl',
+		      resolve: {
+		        bookingTimes: function () {
+		          // return $scope.items;
+		          return theBooking;
+		        }
+		      }
+		    });
+
+		    modalInstance.result.then(function (selectedItem) {
+		      // $scope.selected = selectedItem;
+		    }, function () {
+		      // $log.info('Modal dismissed at: ' + new Date());
+		    });
+
 		};
 
 		// custom texts
@@ -77,4 +101,46 @@ angular.module('myApp', ['mwl.calendar', 'ui.bootstrap'])
 		$scope.$on('$destroy', function($scope) {
 			calendarConfig.templates.calendarMonthCell = 'mwl/calendarMonthCell.html';
 		});*/
-	}]);
+	}])
+	.controller('bookingCtrl', function ($scope, $uibModalInstance, bookingTimes, moment) {
+		$scope.bookingsToday = [
+			{
+				timeFrom: moment(9, 'hours').toDate(),
+				timeTill: moment(9, 'hours').toDate(),
+			},{
+				timeFrom: moment(10, 'hours').toDate(),
+				timeTill: moment(10, 'hours').toDate(),
+			},{
+				timeFrom: moment(11, 'hours').toDate(),
+				timeTill: moment(11, 'hours').toDate(),
+			},{
+				timeFrom: moment(12, 'hours').toDate(),
+				timeTill: moment(12, 'hours').toDate(),
+			},{
+				timeFrom: moment(13, 'hours').toDate(),
+				timeTill: moment(13, 'hours').toDate(),
+			},{
+				timeFrom: moment(14, 'hours').toDate(),
+				timeTill: moment(14, 'hours').toDate(),
+			},{
+				timeFrom: moment(15, 'hours').toDate(),
+				timeTill: moment(15, 'hours').toDate(),
+			},{
+				timeFrom: moment(16, 'hours').toDate(),
+				timeTill: moment(16, 'hours').toDate(),
+			},{
+				timeFrom: moment(17, 'hours').toDate(),
+				timeTill: moment(17, 'hours').toDate(),
+			}
+		];
+		$scope.items = bookingTimes;
+		$scope.selected = {
+			item: $scope.items[0]
+		};
+		$scope.ok = function () {
+			$uibModalInstance.close($scope.selected.item);
+		};
+		$scope.cancel = function () {
+			$uibModalInstance.dismiss('cancel');
+		};
+	});
