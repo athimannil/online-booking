54a90438ec277a8331bedfe5e30d24fc81719d01
Muhammed Athimannil <hello@athimannil.com>
2016-02-16 00:23:29 +0400

    monday weeks start config

--- resources/scripts/core.js
+++ resources/scripts/core.js
@@ -46,6 +46,12 @@ angular.module('myApp', ['ui.router','ui.bootstrap', 'mwl.calendar', 'angularMom
 	console.log("hello mate");
 }])
 .controller('homeCtrl', ['$scope','moment', '$state', function($scope, moment, $state){
+	// Monday week start
+	moment.locale('en', {
+	  week : {
+	    dow : 1 // Monday is the first day of the week
+	  }
+	});
 	$scope.bookingPerDay = 5;
 	var mySchedule = this;
 	$scope.isCellOpen = true; // not working yet
@@ -132,6 +138,27 @@ angular.module('myApp', ['ui.router','ui.bootstrap', 'mwl.calendar', 'angularMom
 		calendarConfig.templates.calendarMonthCell = 'mwl/calendarMonthCell.html';
 	});*/
 }])
+.config(function(calendarConfig) {
+
+    console.log(calendarConfig); //view all available config
+
+    // calendarConfig.templates.calendarMonthView = 'path/to/custom/template.html'; //change the month view template to a custom template
+
+    calendarConfig.dateFormatter = 'moment'; //use either moment or angular to format dates on the calendar. Default angular. Setting this will override any date formats you have already set.
+
+    // calendarConfig.allDateFormats.moment.date.hour = 'HH:mm'; //this will configure times on the day view to display in 24 hour format rather than the default of 12 hour
+
+    // calendarConfig.allDateFormats.moment.title.day = 'ddd D MMM'; //this will configure the day view title to be shorter
+
+    // calendarConfig.i18nStrings.eventsLabel = 'Events'; //This will set the events label on the day view
+
+    // calendarConfig.displayAllMonthEvents = true; //This will display all events on a month view even if they're not in the current month. Default false.
+
+    // calendarConfig.displayEventEndTimes = true; //This will display event end times on the month and year views. Default false.
+
+    // calendarConfig.showTimesOnWeekView = true; //Make the week view more like the day view, with the caveat that event end times are ignored.
+
+})
 .controller('timeCtrl', function ($scope, moment, $state) {
 	$scope.availableBookings = [
 		{
@@ -187,3 +214,4 @@ angular.module('myApp', ['ui.router','ui.bootstrap', 'mwl.calendar', 'angularMom
 .controller('bookCtrl', function ($scope, moment, $state) {
 
 });
+
